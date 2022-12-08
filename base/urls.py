ed108977ced26ad5dd29c35bae8b056b867583f6
Khalid Momand <mohammadkhalidmomand@gmail.com>
2022-12-09 00:44:48 +0400

    Languages Support done

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import (
         home, 
         numbers_to_words,
         numbers_to_speech, 
-        text_to_speech
+        text_to_speech, 
+        get_languages
 )
 
 
@@ -12,5 +13,9 @@ urlpatterns = [
     path('', home, name='homePage'),
     path('numbers_to_words/<int:number>/',numbers_to_words, name='numbers_to_words'),
     path('numbers_to_speech/<int:number>/', numbers_to_speech, name="numbers_to_speech"),
-    path('text_to_speech/<str:text>/', text_to_speech, name='text_to_speech')
+    path('numbers_to_speech/<int:number>/<str:lang>/', numbers_to_speech, name="numbers_to_speech"),
+    path('text_to_speech/<str:text>/', text_to_speech, name='text_to_speech'),  #If request with default language
+    path('text_to_speech/<str:text>/<str:lang>/', text_to_speech, name='text_to_speech'), # if request with specific language
+    path('get_languages/',get_languages, name="get_languages")
+    
 ]
--- base/views.py
+++ base/views.py
@@ -23,7 +23,7 @@ import json
 from num2words import num2words
 
 # import gTTS module to convert text into speech
-from gtts import gTTS
+from gtts import gTTS, langs
 from tempfile import TemporaryFile
 
 
@@ -34,9 +34,10 @@ def home(request):
     context = {
         "Home": "Welcome to Converter API Application",
          "URLs": {
-             "Numbers To Words": '/numbers_to_words/23235532323/',
-             "Text To Speech" : '/text_to_speech/text/',
-             "Numbers To Speech" : '/numbers_to_speech/2232323/',
+             "Numbers To Words": '/numbers_to_words/23235532323/', 
+             "Get Languages": '/get_languages/',
+             "Text To Speech" : '/text_to_speech/text/en/',
+             "Numbers To Speech" : '/numbers_to_speech/2232323/en/',
              "Speech To Text" : '',
          }
     }
@@ -77,11 +78,12 @@ def numbers_to_words(request, number):
 # convert numbers to speech
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
-def numbers_to_speech(request, number):
+def numbers_to_speech(request, number, lang = ''):
     # now hard code the language but in future make it dynamic let the user to choose the language
     language = 'en'
 
-    
+    if lang and type(lang) == str:
+        language = lang
     
     if type(number) == int:
         target_number = int(number)
@@ -113,12 +115,15 @@ def numbers_to_speech(request, number):
 # convert text to speech
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
-def text_to_speech(request, text):
+def text_to_speech(request, text, lang = ""):
     # now hard code the language but in future make it dynamic let the user to choose the language
     language = 'en'
-
     
     
+    
+    # Check if the user spicify the language or not
+    if lang and type(lang) == str:
+        language = lang
     if type(text) == str:
         target_text = str(text)
         
@@ -141,3 +146,16 @@ def text_to_speech(request, text):
     
 
     return JsonResponse({"Error": "Invalid Param!", "Solution": "Provide Integer Number (22222) only."}, safe=False)
+
+
+
+
+
+
+# Get Languages
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def get_languages(request):
+    
+    if request.method == "GET":
+        return JsonResponse(langs._langs, safe=False)
